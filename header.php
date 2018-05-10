45a7cbeb703f5c3162f0089ba2d586db8c234c91
Adnatull <adnaan.masum@yahoo.com>
2018-05-10 23:18:40 +0600

    Added profile page but incomplete

--- header.php
+++ header.php
@@ -9,6 +9,10 @@
     <link rel="stylesheet" type="text/css" media="screen" href="assets/main.css" />
     <script src="assets/main.js"></script>
 
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 
 </head>
 <body>
--- navbar.php
+++ navbar.php
@@ -2,9 +2,35 @@
   <a class="active" href="index.php">Home</a>
   <a href="#news">News</a>
   <a href="#contact">Contact</a>
-  <div class="topnav-right">    
+  <div class="topnav-right"> 
+      <a>
+      <?php
+          echo '<script type="text/javascript">
+          var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+          var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+          var d = new Date();
+          var day = days[d.getDay()];
+          var hr = d.getHours();
+          var min = d.getMinutes();
+          if (min < 10) {
+            min = "0" + min;
+          }
+          var ampm = "am";
+          if( hr > 12 ) {
+              hr -= 12;
+              ampm = "pm";
+          }
+          var date = d.getDate();
+          var month = months[d.getMonth()];
+          var year = d.getFullYear();
+          var x = document.getElementById("time");
+          document.write("<b>" + day + " " + hr + ":" + min + ampm + " " + date + " " + month + " " + year + "</b>");
+          </script>';                
+      ?>
+      </a>
         <?php if (isset($_SESSION["username"])): ?>
-            <a> <?php echo $_SESSION['username']; ?> </a>
+            <a href = "profile.php"> <?php echo $_SESSION['username']; ?> </a>
             <a href = "index.php?logout='1'" style="color: red;">Logout</a>
         <?php else: ?>
             <a href="register.php">Register</a>
