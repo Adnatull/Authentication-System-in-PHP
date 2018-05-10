9fb133266f06e3256a7532e8d554e6565b414431
Adnatull <adnaan.masum@yahoo.com>
2018-05-10 21:50:07 +0600

    Update Navbar

--- header.php
+++ header.php
@@ -4,29 +4,11 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Home</title>
+    <title>Authentication System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="assets/main.css" />
     <script src="assets/main.js"></script>
 
 
-       <!-- Bootstrap core CSS -->
-       <link href="assets/bootstrap.min.css" rel="stylesheet">
-
-        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <link href="assets/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-        <!-- Custom styles for this template -->
-        <link href="assets/navbar.css" rel="stylesheet">
-
-        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-        <!--[if lt IE 9]><script src="assets/ie8-responsive-file-warning.js"></script><![endif]-->
-        <script src="assets/ie-emulation-modes-warning.js"></script>
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-        <script src="assets/html5shiv.min.js"></script>
-        <script src="assets/respond.min.js"></script>
-        <![endif]-->
 </head>
 <body>
